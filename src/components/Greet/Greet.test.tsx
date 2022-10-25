f069739c746cb2931cbe20136c928881e46b75f7
MohJamal <mohjamalalkhatib96@gmail.com>
2022-10-26 02:19:12 +0300

    Code Coverage

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -133,16 +133,16 @@ describe("Greet", () => {
   test("renders correctly", () => {
     render(<Greet />);
 
-    const textElement = screen.getByText("Hello");
+    const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
   });
 });
 
-describe("Nested", () => {
-  test("renders with a name", () => {
-    render(<Greet name="Mohammed" />);
+// describe("Nested", () => {
+//   test("renders with a name", () => {
+//     render(<Greet name="Mohammed" />);
 
-    const textElement = screen.getByText("Hello Mohammed");
-    expect(textElement).toBeInTheDocument();
-  });
-});
+//     const textElement = screen.getByText("Hello Mohammed");
+//     expect(textElement).toBeInTheDocument();
+//   });
+// });
