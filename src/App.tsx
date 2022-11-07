36ae9bda80e8b7b466522f1d3a6c96244120af20
MohJamal <mohjamalalkhatib96@gmail.com>
2022-11-07 02:15:51 +0200

    Pointer Interactions

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Application } from "./components/application/Application";
 import { Skills } from "./components/skills/Skills";
+import { Counter } from "./components/counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Application />
-      <Skills skills={["HTML", "CSS"]} />
+      {/* <Application />
+      <Skills skills={["HTML", "CSS"]} /> */}
+      <Counter />
     </div>
   );
 }
