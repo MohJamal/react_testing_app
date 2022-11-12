646bc1857d4239f072a9438291544cd41d560efe
MohJamal <mohjamalalkhatib96@gmail.com>
2022-11-12 21:20:52 +0200

    Providers

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import "./App.css";
 import { Application } from "./components/application/Application";
 import { Skills } from "./components/skills/Skills";
 import { Counter } from "./components/counter/Counter";
+import { AppProviders } from "./providers/AppProviders";
+import { MuiMode } from "./components/mui/MuiMode";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Application />
+    <AppProviders>
+      <div className="App">
+        {/* <Application />
       <Skills skills={["HTML", "CSS"]} /> */}
-      <Counter />
-    </div>
+        {/* <Counter /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
